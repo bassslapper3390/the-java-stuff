2af63220ee4b450aa21af4c51277f0aa8269a097
piyush <piyush2715@hotmail.com>
2023-07-16 01:34:07 +0530

    This is the first update

---
+++ Loops/demoloop.java
@@ -0,0 +1,16 @@
+package Loops;
+import java.util.*; 
+public class demoloop {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number ");  
+        int a= sc.nextInt(); 
+        if(a%2==0)
+        System.out.println("This is an even number");
+        else if(a==0)
+        System.out.println("This is a zero");
+        else
+        System.out.println("This is an odd number");
+        sc.close();
+    }
+}

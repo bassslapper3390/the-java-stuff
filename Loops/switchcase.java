f93739cb9188c750a2a66c2ccabe1f3ce08a2d7b
piyush <piyush2715@hotmail.com>
2023-07-17 01:23:48 +0530

    forgot the break statement

--- Loops/switchcase.java
+++ Loops/switchcase.java
@@ -1,6 +1,6 @@
 package Loops;
 
-import java.util.Scanner;
+import java.util.*; 
 
 public class switchcase {
     public static void main(String a[]){
@@ -10,19 +10,26 @@ public class switchcase {
         sc.close();
         switch(n){
             case 1:
-            System.out.println("Monday");
+                System.out.println("Monday");
+                break;
             case 2:
-            System.out.println("Tuesday");
+                System.out.println("Tuesday");
+                break;
             case 3:
-            System.out.println("Wednesday");
+                System.out.println("Wednesday");
+                break;
             case 4:
-            System.out.println("Thursday");
+                System.out.println("Thursday");
+                break;
             case 5:
-            System.out.println("Friday");
+                System.out.println("Friday");
+                break;
             case 6:
-            System.out.println("Saturday");
+                System.out.println("Saturday");
+                break;
             case 7:
-            System.out.println("Sunday");
+                System.out.println("Sunday"); 
+                break;
         }
 
     }
